2b864ca4216bdd12a238a1d57223e2afee9ccbf4
GeraldGrassious <nitinramesh2010@gmail.com>
2026-07-16 15:15:35 -0400

    Exit Button Works Now

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    frame: false
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
   });
   win.loadFile('index.html');
   win.setMenuBarVisibility(false);
