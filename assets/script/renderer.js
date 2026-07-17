33236f8c64fe18aeb78263ae74564841051d32f2
David Fu <d4nplus6@gmail.com>
2026-07-17 14:26:21 -0400

    saved state

--- assets/script/renderer.js
+++ assets/script/renderer.js
@@ -2,10 +2,17 @@
 // Import required modules
 const { ipcRenderer } = require('electron');
 // HTML targets
-const TOGGLE_SWITCH = document.getElementById('toggle');
 const LOGO = document.getElementById('logo');
+// HTML targets / buttons
+const TOGGLE_SWITCH = document.getElementById('toggle');
+const BTN_SCHEDULE_TAB = document.getElementById('btn-schedule-tab');
+const BTN_IMAGES_TAB = document.getElementById('btn-images-tab');
+const BTN_AUDIO_TAB = document.getElementById('btn-audio-tab');
+// HTML targets / buttons / images tab
 const BROWSE_BUTTON = document.getElementById("browse-button");
 
+// Attach event listeners
+/** change logos upon toggle switch button press */
 TOGGLE_SWITCH.addEventListener('change', () => {
     ipcRenderer.send('TOGGLE', TOGGLE_SWITCH.checked);
 
@@ -13,6 +20,7 @@ TOGGLE_SWITCH.addEventListener('change', () => {
     else LOGO.src = "assets/img/global/icon_off.png";
 })
 
+/** browse for image to add as reminder overlay */
 BROWSE_BUTTON.addEventListener('click', () => {
     ipcRenderer.send('BROWSE');
     console.log("rizz");
