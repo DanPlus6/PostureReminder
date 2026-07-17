d97e5803e56e4950a63ccff3ca9357d55373e212
David Fu <d4nplus6@gmail.com>
2026-07-17 09:39:23 -0400

    Added comments to main.js and fixed indentations

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
+'use strict';
+// Import required libraries
 const {app, ipcMain, BrowserWindow} = require('electron');
 
+/** Main callback to create the windows */
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 1280,
-    height:800,
-    frame: false,
-    titleBarStyle: 'hidden',
-    titleBarOverlay: {
-        color: '#2b2b2b',       // The background color of the OS control area
-        symbolColor: '#FFFFFF',
-        height: 40
-    },
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  });
-  win.loadFile('index.html');
-  win.setMenuBarVisibility(false);
+	const win = new BrowserWindow({
+		width: 1280,
+		height:800,
+		frame: false,
+		titleBarStyle: 'hidden',
+		titleBarOverlay: {
+			color: '#2b2b2b',       // The background color of the OS control area
+			symbolColor: '#FFFFFF',
+			height: 40
+		},
+		webPreferences: {
+		nodeIntegration: true,
+		contextIsolation: false
+		}
+	});
+	win.loadFile('index.html');
+	win.setMenuBarVisibility(true);
 }
 
-ipcMain.on('EXIT', () => {
-  app.quit();
-});
+// exit button
+ipcMain.on('EXIT', () => { app.quit(); });
 
-
+// initialize electron and create window
 app.whenReady().then(createWindow);
