ce130f1a39277426a08adb54855a87f635affa21
David Fu <d4nplus6@gmail.com>
2026-07-17 11:05:36 -0400

    Added system tray icon & fixed taskbar icon

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,22 +1,32 @@
 'use strict';
 // Import required modules
-const {app, ipcMain, BrowserWindow} = require('electron');
+const {app, ipcMain, BrowserWindow, nativeImage, Tray} = require('electron');
 const path = require('path');
 
-/** Main callback to create the windows */
+/** @type {BrowserWindow} main window object */
+let win;
+/** @type {Tray} system tray icon object */
+let tray;
+
+/** Main callback to create the window */
 function createWindow() {
-	const win = new BrowserWindow({
+	// create window
+	win = new BrowserWindow({
 		width: 1280,
 		height: 800,
 		titleBarStyle: 'hidden',
-		icon: path.join(__dirname, 'assets/img/icon.png'),
+		icon: nativeImage.createFromPath(path.join(__dirname, "../img/icon.png")),
 		...(process.platform != 'darwin' ? { titleBarOverlay: {
 			color: '#2b2b2b',
 			symbolColor: '#FFFFFF',
 			height: 65,
 		} } : {})
 	});
+	// load html file for app
 	win.loadFile('index.html');
+
+	// create system tray icon
+	tray = new Tray(path.join(__dirname, "../img/icon.png"));
 }
 
 // exit button
